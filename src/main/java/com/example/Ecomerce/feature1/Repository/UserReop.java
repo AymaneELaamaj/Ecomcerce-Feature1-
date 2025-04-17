a76bef0b990af3e13ad553323c2cbd20a9f89eea
AymaneElaamaj <aymaneamg300@gmail.com>
2025-04-17 21:47:54 +0100

    add User Repository

---
+++ src/main/java/com/example/Ecomerce/feature1/Repository/UserReop.java
@@ -0,0 +1,7 @@
+package com.example.Ecomerce.feature1.Repository;
+
+import com.example.Ecomerce.feature1.Model.Utilsateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReop extends JpaRepository<Utilsateur,Long> {
+}
