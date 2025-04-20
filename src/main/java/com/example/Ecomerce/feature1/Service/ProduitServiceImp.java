6f2cea1e4b2e6807370d94e26f5b22066787f14b
AymaneElaamaj <aymaneamg300@gmail.com>
2025-04-20 01:54:41 +0100

    changes

--- src/main/java/com/example/Ecomerce/feature1/Service/ProduitServiceImp.java
+++ src/main/java/com/example/Ecomerce/feature1/Service/ProduitServiceImp.java
@@ -9,11 +9,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 @Service
-public class ProduitServiceImp implements IProduitService {
-    public ProduitServiceImp(ProduitReopsitory produitRepository) {
-        this.produitRepository = produitRepository;
-    }
 
+public class ProduitServiceImp implements IProduitService {
     @Autowired
     private ProduitReopsitory produitRepository;
     @Override
