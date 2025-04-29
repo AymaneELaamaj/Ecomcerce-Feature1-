f6948cdeb1dddba8c8b992548ce9bcaebf1cf4fe
AymaneElaamaj <aymaneamg300@gmail.com>
2025-04-29 17:15:13 +0100

    securisé l'acce a Payment

--- src/main/java/com/example/Ecomerce/feature1/controller/PaymentController.java
+++ src/main/java/com/example/Ecomerce/feature1/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.example.Ecomerce.feature1.Model.Payment;
 import com.example.Ecomerce.feature1.Service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_VENDOR')")
     public ResponseEntity<Payment> pay(@RequestBody PaymentDTO paymentDTO) {
         Payment payment = paymentService.process(paymentDTO);
         return ResponseEntity.ok(payment);
